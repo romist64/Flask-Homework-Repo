bac8ff1f44070f65d5a2e4fedfe4f6c0cd38733c
romist64 <45908283+romist64@users.noreply.github.com>
2025-03-11 08:15:58 -0500

    Add files via upload

--- Unit 1/homework/theproject.py
+++ Unit 1/homework/theproject.py
@@ -5,7 +5,7 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def hello():
-   message = "Hello, Flask! <a href='/Two'>Goodbye!</a>"
+   message = "Hello, Flask! <a href='/Two'>Goodbye!</a><br><a href='/form'>Form</a>"
    return message
 
 @app.route('/Two')
@@ -22,7 +22,14 @@ def results():
    color = request.form['color']
    luck_num = request.form['luck_num']
    fav_class = request.form['fav_class']
-   best_pix = request.form['best_pix']
+   best_pix = request.form['best_pix'].lower().strip()
+   films = ["toy story","a bug's life","toy story 2","monsters, inc.",
+      "finding nemo", "the incredibles","cars","ratatouille","wall-e","up",
+      "toy story 3","cars 2", "brave","monsters university","inside out",
+      "the good dinosaur","finding dory", "cars 3","coco","incredibles 2",
+      "toy story 4","onward","soul"]
+   if best_pix not in films:
+      best_pix = "Sorry, '{0}' is not a Pixar film.".format(best_pix.title())
    return render_template("results.html", color=color, luck_num=luck_num, fav_class=fav_class, best_pix=best_pix)
 
 if __name__ == '__main__':
